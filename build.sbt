cb601d406aec3adeb2e3044223fd376b883e673c
Gary Malouf <gary@garymalouf.com>
2013-02-18 23:24:05 -0500

    Make log4j a test dependency and change logger to depend on slf4j solely.

--- build.sbt
+++ build.sbt
@@ -7,10 +7,13 @@ version := "0.1-SNAPSHOT"
 scalaVersion := "2.10.0"
 
 libraryDependencies ++= Seq(
-	"org.scala-tools.testing" %% "specs" % "1.6.9" % "test",
 	"me.prettyprint" % "hector-core" % "1.0-5" exclude(
       "junit", "junit") exclude("org.slf4j", "slf4j-api"),
-	"org.slf4j" % "slf4j-log4j12" % "1.7.2"
+	"org.slf4j" % "slf4j-api" % "1.7.2",
+	"com.chuusai" %% "shapeless" % "1.2.3",
+	"org.scala-tools.testing" %% "specs" % "1.6.9" % "test",
+	"org.slf4j" % "slf4j-log4j12" % "1.7.2" % "test"
+	
 )
 
 scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature", "-language:implicitConversions") 
--- src/main/scala/util/Logging.scala
+++ src/main/scala/util/Logging.scala
@@ -1,30 +1,33 @@
 package github.joestein.util
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 /**
- * LogHelper is a trait you can mix in to provide easy log4j logging 
- * for your scala classes. 
- **/
+ * LogHelper is a trait you can mix in to provide easy slf4j logging
+ * for your scala classes.
+ */
 trait LogHelper {
-    val loggerName = this.getClass.getName
-    lazy val logger = Logger.getLogger(loggerName)
+  val loggerName = this.getClass.getName
+  lazy val logger = LoggerFactory.getLogger(loggerName)
 
-	def debug(msg: => String) {
-		if (logger.isDebugEnabled())
-			logger.debug(msg)
-	}
-	def info(msg: => String) {
-		if (logger.isInfoEnabled())
-			logger.info(msg)
-	}
-	def error(msg: => String) {
-		logger.error(msg)
-	}		
-	def fatal(msg: => String) {
-		logger.fatal(msg)
-	}	
-	def warn(msg: => String) {
-		logger.warn(msg)
-	}	
+  def debug(msg: => String) {
+    if (logger.isDebugEnabled())
+      logger.debug(msg)
+  }
+  def info(msg: => String) {
+    if (logger.isInfoEnabled())
+      logger.info(msg)
+  }
+  def error(msg: => String) {
+    logger.error(msg)
+  }
+
+  def fatal(msg: => String) {
+    logger.error(msg)
+  }
+
+  def warn(msg: => String) {
+    logger.warn(msg)
+  }
 }
